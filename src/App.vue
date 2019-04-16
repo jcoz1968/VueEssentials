2c3267317cc896c7f454ecaea9f501996ab95210
Jeffery Cosby <jcoz1968@gmail.com>
2019-04-15 19:58:01 -0500

    using chrome dev tools

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,40 @@
 <template>
   <div id="app" class="container mt-5">
     <h1>My Shop</h1>
-    <product-list :maximum="maximum" :products="products"></product-list>
+    <product-list :maximum="maximum" :products="products" @add="addItem"></product-list>
   </div>
 </template>
 
 <script>
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { ProductList } from "./components/ProductList"
+import ProductList from "./components/ProductList.vue";
 
 export default {
   name: "app",
   data: function() {
     return {
       maximum: 99,
+      cart: [],
       products: null
+    };
+  },
+  methods: {
+    addItem: function(product) {
+      var whichProduct;
+      var existing = this.cart.filter(function(item, index) {
+        if (item.product.id == Number(product.id)) {
+          whichProduct = index;
+          return true;
+        } else {
+          return false;
+        }
+      });
+
+      if (existing.length) {
+        this.cart[whichProduct].qty++;
+      } else {
+        this.cart.push({ product: product, qty: 1 });
+      }
     }
   },
   components: {
@@ -22,12 +42,11 @@ export default {
     ProductList
   },
   mounted: function() {
-    fetch('https://hplussport.com/api/products/order/price')
-    .then(response => response.json())
-    .then(data => {
-      this.products = data;
-    })
-  }  
+    fetch("https://hplussport.com/api/products/order/price")
+      .then(response => response.json())
+      .then(data => {
+        this.products = data;
+      });
+  }
 };
 </script>
-
